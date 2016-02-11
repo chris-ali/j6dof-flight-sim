6d046b86327ebd27fc14036a79152784aef17129
Christopher Ali <christopher.ali@outlook.com>
2016-02-10 22:04:41 -0500

    More Work to Trimming Method
    - Continued work on Trimming class; need to create a copy of
    InitialConditions and InitialControls text files for intial setup of
    trimming
    
    - Added case for Integrate6DOFEquations to use different text files with
    IntegrationSetup parsing methods when in TRIM_MODE; these other files
    will be modified after each step with new initial conditions and
    controls to find the trim point of the simulation
    
    - Changed FILE_PATH to protected in IntegrationSetup

--- src/com/chrisali/javaflightsim/setup/Trimming.java
+++ src/com/chrisali/javaflightsim/setup/Trimming.java
@@ -1,8 +1,16 @@
 package com.chrisali.javaflightsim.setup;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.EnumSet;
+import java.util.Map;
 
 import com.chrisali.javaflightsim.aircraft.AircraftBuilder;
+import com.chrisali.javaflightsim.controls.FlightControls;
 import com.chrisali.javaflightsim.utilities.integration.Integrate6DOFEquations;
 import com.chrisali.javaflightsim.utilities.integration.SimOuts;
 
@@ -26,34 +34,65 @@ public class Trimming {
 			
 			Integrate6DOFEquations runSim = new Integrate6DOFEquations(ab,EnumSet.of(Options.TRIM_MODE));
 			
-			double[] initialConditions = new double[]{runSim.getSimOut().get(SimOuts.U),
-													  runSim.getSimOut().get(SimOuts.V),
-													  runSim.getSimOut().get(SimOuts.W),
-												  	  runSim.getSimOut().get(SimOuts.PHI),
-												  	  runSim.getSimOut().get(SimOuts.THETA),
-												  	  runSim.getSimOut().get(SimOuts.P),
-												  	  runSim.getSimOut().get(SimOuts.Q),
-												  	  runSim.getSimOut().get(SimOuts.R)};
+			EnumMap<InitialConditions, Double> nextinitialConditions = new EnumMap<InitialConditions, Double>(InitialConditions.class);
+			nextinitialConditions.put(InitialConditions.INITU,     	runSim.getSimOut().get(SimOuts.U));
+			nextinitialConditions.put(InitialConditions.INITV, 	  	runSim.getSimOut().get(SimOuts.V));
+			nextinitialConditions.put(InitialConditions.INITW, 	  	runSim.getSimOut().get(SimOuts.W));
+			nextinitialConditions.put(InitialConditions.INITPHI,   	runSim.getSimOut().get(SimOuts.PHI));
+			nextinitialConditions.put(InitialConditions.INITTHETA, 	runSim.getSimOut().get(SimOuts.THETA));
+			nextinitialConditions.put(InitialConditions.INITP, 	  	runSim.getSimOut().get(SimOuts.P));
+			nextinitialConditions.put(InitialConditions.INITQ, 	  	runSim.getSimOut().get(SimOuts.Q));
+			nextinitialConditions.put(InitialConditions.INITR, 	  	runSim.getSimOut().get(SimOuts.R));
 			
-			double[] initialControls = new double[]{runSim.getSimOut().get(SimOuts.ELEVATOR),
-													runSim.getSimOut().get(SimOuts.AILERON),
-													runSim.getSimOut().get(SimOuts.RUDDER),
-													runSim.getSimOut().get(SimOuts.THROTTLE_1),
-													runSim.getSimOut().get(SimOuts.THROTTLE_2),
-													runSim.getSimOut().get(SimOuts.THROTTLE_3),
-													runSim.getSimOut().get(SimOuts.THROTTLE_4)};
+			EnumMap<FlightControls, Double> nextinitialControls = new EnumMap<FlightControls, Double>(FlightControls.class);
+			nextinitialControls.put(FlightControls.ELEVATOR,   runSim.getSimOut().get(SimOuts.ELEVATOR));
+			nextinitialControls.put(FlightControls.AILERON,    runSim.getSimOut().get(SimOuts.AILERON));
+			nextinitialControls.put(FlightControls.RUDDER, 	   runSim.getSimOut().get(SimOuts.RUDDER));
+			nextinitialControls.put(FlightControls.THROTTLE_1, runSim.getSimOut().get(SimOuts.THROTTLE_1));
+			nextinitialControls.put(FlightControls.THROTTLE_2, runSim.getSimOut().get(SimOuts.THROTTLE_2));
+			nextinitialControls.put(FlightControls.THROTTLE_3, runSim.getSimOut().get(SimOuts.THROTTLE_3));
+			nextinitialControls.put(FlightControls.THROTTLE_4, runSim.getSimOut().get(SimOuts.THROTTLE_4));
 			
-			writeInitialConditions(initialConditions);
+			writeNextInitialConditions(nextinitialConditions);
 			
-			writeInitialControls(initialControls);
+			writeNextInitialControls(nextinitialControls);
 		}
 	}
 
-	public static void writeInitialConditions(double[] initialConditions) {
-		IntegrationSetup.gatherInitialConditions("InitialConditions");
+	private static void writeNextInitialConditions(EnumMap<InitialConditions, Double> nextInitialConditions) {
+		EnumMap<InitialConditions, Double> initialConditions = IntegrationSetup.gatherInitialConditions("InitialConditions");
+		
+		// nextStepInitialConditions only contains part of the whole initialConditions EnumMap, because
+		// the other states are position values that don't influence the trim results - loop through the
+		// the states that do influence trimming, and assign them to initialConditions 
+		for (Map.Entry<InitialConditions, Double> entry : nextInitialConditions.entrySet())
+			initialConditions.put(entry.getKey(), entry.getValue());
+			
+		writeFile(initialConditions, "nextStepInitialConditions");
 	}
 
-	public static void writeInitialControls(double[] initialControls) {
-		IntegrationSetup.gatherInitialControls("InitialControls");
+	private static void writeNextInitialControls(EnumMap<FlightControls, Double> nextInitialControls) {
+		EnumMap<FlightControls, Double> initialControls = IntegrationSetup.gatherInitialControls("InitialControls");
+		
+		for (Map.Entry<FlightControls, Double> entry : nextInitialControls.entrySet())
+			initialControls.put(entry.getKey(), entry.getValue());
+		
+		writeFile(initialControls, "nextStepInitialControls");
+	}
+	
+	private static void copyFile(String fileName) {
+		
+	}
+	
+	private static void writeFile(EnumMap<?, Double> initEnumMap, String fileName) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(IntegrationSetup.FILE_PATH).append(fileName).append(".txt");
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(sb.toString()))) {
+			for(Enum<?> entry : initEnumMap.keySet())
+				bw.write(entry.toString() + " = " + initEnumMap.get(entry));
+		} catch (FileNotFoundException e) {System.err.println("Could not find: " + fileName + ".txt!");}
+		catch (IOException e) {System.err.println("Could not read: " + fileName + ".txt!");}
+		catch (NullPointerException e) {System.err.println("Bad reference to: " + fileName + ".txt!");} 
 	}
 }
