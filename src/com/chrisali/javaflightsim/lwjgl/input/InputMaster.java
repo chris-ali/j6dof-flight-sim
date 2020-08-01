05a7582a5dec989a8450eccd0711858e7fb6890f
Christopher <christopher.ali@protonmail.com>
2020-08-01 12:53:35 -0400

    Reconfigure flight control inputs to accept external sources - Updated InputMaster to handle joystick axes and buttons - Still need to figure out better way to handle keyboard / joystick hat values

--- src/com/chrisali/javaflightsim/lwjgl/input/InputMaster.java
+++ src/com/chrisali/javaflightsim/lwjgl/input/InputMaster.java
@@ -25,7 +25,9 @@ import java.util.Set;
 import com.chrisali.javaflightsim.simulation.datatransfer.InputData;
 import com.chrisali.javaflightsim.simulation.flightcontrols.FlightControl;
 import com.chrisali.javaflightsim.simulation.setup.ControlsConfiguration;
+import com.chrisali.javaflightsim.simulation.setup.JoystickAxis;
 import com.chrisali.javaflightsim.simulation.setup.KeyCommand;
+import com.chrisali.javaflightsim.simulation.setup.ControlsConfiguration.JoystickAssignments;
 import com.chrisali.javaflightsim.simulation.utilities.FileUtilities;
 
 import org.apache.logging.log4j.LogManager;
@@ -65,7 +67,6 @@ public class InputMaster {
 
     public static void update() {
         //TODO Revise (de)serialization of controls to use int values for keys/axes that are "legible" for LWJGL Keyboard and Controller classes
-        /*
         for (Map.Entry<String, KeyCommand> entry : controlsConfig.getKeyboardAssignments().entrySet()) {
             if (Keyboard.isKeyDown(key))
                 inputData.addKeyPressed(entry.getValue());
@@ -73,10 +74,47 @@ public class InputMaster {
 
         Controllers.poll();
 
-        for (int i = 0; i < controllerCount; i++) {
+        Map<String, JoystickAssignments> allJoystickAssignments = controlsConfig.getJoystickAssignments();
 
+        // Loop through all conntected controllers
+        for (int controllerIndex = 0; controllerIndex < controllerCount; controllerIndex++) {
+            Controller controller = Controllers.getController(controllerIndex);
+
+            // Get assignments for a single joystick
+            JoystickAssignments joystickAssignments = allJoystickAssignments.get(controller.getName());
+            
+            // Loop through joystick axes on connected controller
+            for (int axisIndex; axisIndex < controller.getAxisCount(); axisIndex++) {
+                JoystickAxis axis = joystickAssignments.getAxisAssignments().get(controller.getAxisName(axisIndex));
+
+                // If controls configuration has an assignment for this axis, use it
+                if (axis != null)
+                    inputData.updateJoystickInputs(axis.getAxisAssignment(), controller.getAxisValue(axisIndex));
+            }
+
+            // Loop through buttons on connected controller
+            for (int buttonIndex= 0; buttonIndex< controller.getButtonCount(); buttonIndex++) {
+                Map<String, KeyCommand> buttonAssignments = joystickAssignments.getButtonAssignments();
+                
+                // If controls configuration has an assignment for this pressed button, use it
+                if (controller.isButtonPressed(buttonIndex)) {
+                    KeyCommand command = buttonAssignments.get(controller.getButtonName(buttonIndex));
+
+                    if (command != null)
+                        inputData.addKeyPressed(command);
+                }
+            }
+
+            // May need to reassign hat to use POV X and Y in controls config
+            Map<Float, KeyCommand> hatAssignments = joystickAssignments.getHatAssignments();
+            if (hatAssignments != null) {
+                float hatVal = controller.getPovX();
+                KeyCommand command = hatAssignments.get(hatVal);
+
+                if (hatVal > 0  && command != null)
+                    inputData.addKeyPressed(command);
+            }
         }
-        */
     }
 
 	public static InputData getInputData() { 
